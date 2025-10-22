f576b5b336027397e6886ac4e0d83e916e9e1555
Adil <mdi256129@gmail.com>
2025-10-22 16:57:59 +0600

    Day 10: Add else blocks to handle two paths in logic

---
+++ Day-10-if-else.js
@@ -0,0 +1,41 @@
+let age = 16;
+
+if (age >=18) {
+    console.log("You're an adult!");  // This path runs if condition is TRUE
+} else {
+    console.log("You're a minor!");   // This path if runs if condition is FALSE
+}
+
+// output: You're a minor!
+
+let number = 5;
+
+if (number > 0) {
+    console.log(number + " is positive")
+} else {
+    console.log(number + " is negative");
+}
+
+let password = "secret123";
+
+if (password === "secret123") {
+    console.log("Access granted!");
+} else {
+    console.log("Access denied!");
+}
+
+let temperature = 25;
+
+if (temperature >= 25) {
+    console.log("It's hot! Wear shorts.");
+} else {
+    console.log("It's cold! Wear a jacket.");
+}
+
+let score = 75;
+
+if (score >= 60) {
+    console.log(score + " - You passes!");
+} else {
+    console.log(score + " - You failed. Study more!");
+}
