32ad4730be812de681734063cb90567deb00cee1
Adil <mdi256129@gmail.com>
2025-12-19 14:06:30 +0600

    Day 29: Array basics and indexing practice

---
+++ Day-29-Array-Basics.js
@@ -0,0 +1,31 @@
+// Exercise 1: My first array
+
+let myFavoriteFoods = ["pizza", "ramen", "sushi", "burger", "sandwich"];
+
+console.log(myFavoriteFoods[0]);
+console.log(myFavoriteFoods[2]);
+
+// Exercise 2: Numbers array
+
+let numbers = [10, 20, 30, 40, 50];
+
+console.log(numbers[0]);
+console.log(numbers[4]);
+console.log(numbers[2]);
+
+// Exercise 3: Mixed Practice
+
+let students = ["Anika", "Rafi", "Sara", "Karim"];
+
+console.log(`Student 1: ${students[0]}`)
+console.log(`Student 2: ${students[1]}`);
+console.log(`Student 3: ${students[2]}`);
+console.log(`Student 4: ${students[3]}`);
+
+// Exercise 4: Boolean Array
+
+let quizAnswers = [true, false, true, true, false];
+
+console.log(`Question 1: ${quizAnswers[0]}`);
+console.log(`Question 3: ${quizAnswers[2]}`);
+console.log(`Question 5: ${quizAnswers[4]}`);
