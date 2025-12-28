044854dc085be53456f7777555d63101b5ac7364
Adil <mdi256129@gmail.com>
2025-12-28 18:18:47 +0600

    Remove messingaround.js - practice file no longer needed

--- messingaround.js
+++
@@ -1,8 +0,0 @@
-function introduce(firstName, lastName, age) {
-    return `meet ${firstName} ${lastName}, he's ${age} years old 🧃💀`;
-}
-
-let intro = introduce('adil', '🗿', 17); // i don't like last name 💀
-
-console.log(intro);
-console.log(introduce('sponge', 'bob', 16));
